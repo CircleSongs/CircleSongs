c6b9e637b043c3796c7fd5da62b9c9e592ef84b9
Dinshaw <gobhai@gmai.com>
2018-12-21 21:27:46 -0500

    Try and get heroku package

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
-sudo mkdir -p /usr/local/lib /usr/local/bin
-sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
-sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+
+wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+mkdir -p /usr/local/lib /usr/local/bin
+tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
