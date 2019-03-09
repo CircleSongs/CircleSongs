cae54eb7ef43a00c4b67b919deb55ed4e4516d69
Dinshaw <gobhai@gmai.com>
2019-03-09 09:33:23 -0500

    Fix Contact Form defaults * Add NewRelic

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -1,4 +1,8 @@
 class ContactForm < MailForm::Base
+  DEFAULT_FROM_NAME = 'Contact Form'.freeze
+  DEFAULT_FROM_EMAIL = "contact_form@#{ENV['base_domain']}".freeze
+  DEFAULT_SUBJECT = "Hi from #{ENV['base_domain']}!".freeze
+
   attribute :name
   attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i, allow_blank: true
   attribute :subject
@@ -7,9 +11,23 @@ class ContactForm < MailForm::Base
 
   def headers
     {
-      subject: subject,
+      subject: subject_text,
       to: ENV['contact_form_email'],
-      from: %("#{name}" <#{email}>)
+      from: "#{from_name} <#{from_email}>"
     }
+  end
+
+  private
+
+  def from_name
+    name.blank? ? DEFAULT_FROM_NAME : name
+  end
+
+  def from_email
+    email.blank? ? DEFAULT_FROM_EMAIL : email
+  end
+
+  def subject_text
+    subject.blank? ? DEFAULT_SUBJECT : subject
   end
 end
---
+++ spec/models/contact_form_spec.rb
@@ -0,0 +1,47 @@
+RSpec.describe ContactForm do
+  let(:contact_form) { described_class.new(params) }
+  let(:params) do
+    {
+      name: name
+    }
+  end
+
+  describe '::DEFAULT_FROM_EMAIL' do
+    it 'returns a sensible default' do
+      expect(described_class::DEFAULT_FROM_EMAIL).to eq "contact_form@#{ENV['base_domain']}"
+    end
+  end
+
+  describe '::DEFAULT_FROM_NAME' do
+    it 'returns a sensible default' do
+      expect(described_class::DEFAULT_FROM_NAME).to eq 'Contact Form'
+    end
+  end
+
+
+  describe '::DEFAULT_SUBJECT' do
+    it 'returns a sensible default' do
+      expect(described_class::DEFAULT_SUBJECT).to eq <<~SUBJECT.strip
+        Hi from #{ENV['base_domain']}!
+      SUBJECT
+    end
+  end
+
+  describe '#headers[:from]' do
+    context 'with a name' do
+      let(:name) { FFaker::Name.name }
+
+      it 'returns the name' do
+        expect(contact_form.headers[:from]).to match name
+      end
+    end
+
+    context 'without a name' do
+      let(:name) { nil }
+
+      it 'returns the default name' do
+        expect(contact_form.headers[:from]).to match ContactForm::DEFAULT_FROM_NAME
+      end
+    end
+  end
+end
