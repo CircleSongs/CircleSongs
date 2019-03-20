8eb05aa3358cc79bfb8fa726015b62cbd39d3745
Dinshaw <gobhai@gmai.com>
2019-03-20 11:07:54 -0600

    Move FA to sass

--- app/javascript/SongChords.js
+++ app/javascript/SongChords.js
@@ -1,14 +1,17 @@
 import ChordSheetJS from "chordsheetjs";
 const node = document.getElementById("song-chords");
-const data = JSON.parse(node.getAttribute("data"));
-const chordSheet = data;
 
-const parser = new ChordSheetJS.ChordProParser();
-const song = parser.parse(chordSheet);
-const formatter = new ChordSheetJS.TextFormatter();
-const disp = formatter.format(song);
+if (node) {
+  const data = JSON.parse(node.getAttribute("data"));
+  const chordSheet = data;
 
-node.innerHTML = disp
+  const parser = new ChordSheetJS.ChordProParser();
+  const song = parser.parse(chordSheet);
+  const formatter = new ChordSheetJS.TextFormatter();
+  const disp = formatter.format(song);
+
+  node.innerHTML = disp
+}
 
 const SongChords = {
   initialize() {
