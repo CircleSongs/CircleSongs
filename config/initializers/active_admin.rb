8bf2a21d7901e01de7548a4a0568cbc312ea6cff
Dinshaw <gobhai@gmai.com>
2020-03-27 16:03:16 -0600

    Admin song chords

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -308,7 +308,7 @@ module AdminPageLayoutOverride
 
     # this will be added at the end of <head>
     within head do
-      text_node(javascript_pack_tag('song-chords'))
+      text_node(javascript_pack_tag('admin-song-chords'))
     end
   end
 end
