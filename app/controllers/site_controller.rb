43ea2dbc55e9178b7db6d17256012530f43e2aad
Dinshaw <gobhai@gmai.com>
2020-07-17 14:02:40 -0600

    Add static content and Vocabulary

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,14 @@
+class SiteController < ApplicationController
+  def quechua
+    @vocabularies = Vocabulary.all
+  end
+
+  def icaros
+  end
+
+  def integration
+  end
+
+  def learning_music
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_31_031520) do
+ActiveRecord::Schema.define(version: 2020_07_17_172446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -95,6 +95,13 @@ ActiveRecord::Schema.define(version: 2020_05_31_031520) do
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "vocabularies", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "text"
+    t.string "translation"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
