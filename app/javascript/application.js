d7cc20c8bf2283cb86ad0192a3451098caee3455
Dinshaw Gobhai <gobhai@gmail.com>
2022-04-30 18:15:24 +0530

    Updated JS with Bootstrap 5

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,17 +1,24 @@
-// require("@rails/ujs").start();
-// require("turbolinks").start();
-// Turbolinks.start();
+import "popper.js";
 
-// import "popper.js";
-
-// import * as bootstrap from "bootstrap";
-// window.bootstrap = bootstrap;
+import * as bootstrap from "bootstrap";
+window.bootstrap = bootstrap;
 
 import "src/song-chords";
 import "src/broken-link-report";
 import "src/chord-forms";
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   $('[data-toggle="tooltip"]').tooltip();
-//   $('[data-toggle="popover"]').popover();
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  var popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="popover"]')
+  );
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
+
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
