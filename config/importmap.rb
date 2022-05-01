379056cbe9353071c31f9b02ed1bebf482190477
Dinshaw Gobhai <gobhai@gmail.com>
2022-05-01 10:22:09 +0530

    Updates to JS and admin
    * Fixed admin song filters
    * Fixed admin js

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
-pin "popper.js", to: "https://ga.jspm.io/npm:popper.js@1.16.1/dist/umd/popper.js"
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.2/lib/index.js"
+pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
 pin "vexchords", to: "https://ga.jspm.io/npm:vexchords@1.2.0/index.js"
 pin "@svgdotjs/svg.js", to: "https://ga.jspm.io/npm:@svgdotjs/svg.js@3.1.2/dist/svg.esm.js"
 pin "chordsheetjs", to: "https://ga.jspm.io/npm:chordsheetjs@6.3.0/lib/index.js"
