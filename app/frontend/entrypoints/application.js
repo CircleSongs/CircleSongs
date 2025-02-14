2ca7f0cf32412096bfef28f5f88111dbaa21ff37
Dinshaw Gobhai <gobhai@gmail.com>
2025-02-14 12:51:36 -0700

    Updates

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,39 +1,4 @@
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-// console.log("Vite ⚡️ Rails");
-
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
-
-// console.log(
-//   "Visit the guide for more information: ",
-//   "https://vite-ruby.netlify.app/guide/rails"
-// );
-
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
-
-// import "../src/broken-link-report";
-
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "@fortawesome/fontawesome-free/css/regular.min.css";
+import "@fortawesome/fontawesome-pro/css/all.min.css";
 import "@popperjs/core";
 import * as bootstrap from "bootstrap";
 import "~/js/chord-forms";
