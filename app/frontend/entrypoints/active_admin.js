9e800b036061b9faf1375e0e1b2da6ae9490d04e
Dinshaw Gobhai <gobhai@gmail.com>
2025-02-15 22:25:10 -0700

    Remove Fontawesome free

--- app/frontend/entrypoints/active_admin.js
+++ app/frontend/entrypoints/active_admin.js
@@ -2,7 +2,7 @@ import $ from "jquery";
 window.$ = $;
 import "jquery-ui-dist/jquery-ui";
 
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-pro/css/all.min.css";
 import "@activeadmin/activeadmin";
 
 import "~/js/chord-forms";
