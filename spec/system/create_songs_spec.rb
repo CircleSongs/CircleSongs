bb2c2c7cbcea7f09e2283ebcf8ad5393f05c8033
Dinshaw Gobhai <gobhai@gmail.com>
2025-02-18 18:41:01 -0700

    Fix specs after name change to composer_name

--- spec/system/create_songs_spec.rb
+++ spec/system/create_songs_spec.rb
@@ -3,7 +3,7 @@ RSpec.feature "As an admin user", type: :system do
   let(:title) { FFaker::Music.song }
   let(:description) { FFaker::Lorem.paragraphs.join("\n") }
   let(:alternate_title) { FFaker::Music.song }
-  let(:composer) { FFaker::Music.song }
+  let(:composer_name) { FFaker::Music.song }
   let(:composer_url) { FFaker::Internet.http_url }
   let(:lyrics) { FFaker::Music.song }
   let(:underlined_lyric) { "This <u>lyric</u> is underlined." }
@@ -21,7 +21,7 @@ RSpec.feature "As an admin user", type: :system do
     click_on "New Song"
     fill_in "Title", with: title
     fill_in "Alternate title", with: alternate_title
-    fill_in "Composer", with: composer
+    fill_in "Composer name", with: composer_name
     fill_in "Composer url", with: composer_url
     fill_in "Lyrics", with: lyrics
     fill_in "Translation", with: translation
@@ -34,13 +34,13 @@ RSpec.feature "As an admin user", type: :system do
     expect(page).to have_content "Song was successfully created."
     expect(page).to have_content title
     expect(page).to have_content alternate_title
-    expect(page).to have_link(composer, href: composer_url)
+    expect(page).to have_link(composer_name, href: composer_url)
     expect(page).to have_content lyrics
     expect(page).to have_content translation
     expect(page).to have_content description
     expect(page).to have_content "Traditional"
     expect(page).to have_content "English and Spanish"
-    expect(page).not_to have_content chords
+    expect(page).to have_no_content chords
     expect(page).to have_content formatted_chords
   end
 
@@ -51,18 +51,18 @@ RSpec.feature "As an admin user", type: :system do
     fill_in "Translation", with: underlined_translation
     click_on "Create Song"
     within ".row-lyrics" do
-      expect(page).to have_selector "u"
+      expect(page).to have_css "u"
     end
     within ".row-translation" do
-      expect(page).to have_selector "u"
+      expect(page).to have_css "u"
     end
     visit songs_path
     click_on title
     within ".lyrics" do
-      expect(page).to have_selector "u"
+      expect(page).to have_css "u"
     end
     within ".translation" do
-      expect(page).to have_selector "u"
+      expect(page).to have_css "u"
     end
   end
 end
