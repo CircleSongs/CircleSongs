79c5fe473b0d99eff84359fcd719bd3443d4655c
Dinshaw Gobhai <gobhai@gmail.com>
2025-03-18 09:37:28 -0600

    Fix song delete

--- spec/system/admin/songs_spec.rb
+++ spec/system/admin/songs_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe "As an admin user" do
   let(:user) { users(:admin) }
   let(:title) { FFaker::Music.song }
   let(:chord_form) { chord_forms(:Am7) }
+  let(:song) { songs(:hotel_california) }
 
   before do
     login_as user
@@ -21,12 +22,30 @@ RSpec.describe "As an admin user" do
     expect(find("img")["src"]).to eq Song.find_by(title: title).image_url(:thumb)
     expect(page).to have_content "Song was successfully created."
     expect(page).to have_content chord_form.chord
-    expect(page).to have_selector "div.chord-form svg"
+    expect(page).to have_css "div.chord-form svg"
   end
 
   scenario "I see an edit link on the public index page" do
     visit songs_path
-    expect(page).to have_selector "a.edit"
-    expect(page).to have_selector ".has-recording"
+    expect(page).to have_css "a.edit"
+    expect(page).to have_css ".has-recording"
+  end
+
+  scenario "I can delete a song from the index page" do
+    visit admin_songs_path
+    within "#song_#{song.id}" do
+      accept_confirm do
+        click_on "Delete"
+      end
+    end
+    expect(page).to have_content "Song was successfully destroyed."
+  end
+
+  scenario "I can delete a song from the show page" do
+    visit admin_song_path(song)
+    accept_confirm do
+      click_on "Delete Song"
+    end
+    expect(page).to have_content "Song was successfully destroyed."
   end
 end
