71989e7ea1e1b4ed2b69436279775f85d3ec856e
Dinshaw Gobhai <gobhai@gmail.com>
2026-02-12 14:41:24 -0700

    Fixed Songs sort by composer name

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,9 +4,9 @@ class SongsController < ApplicationController
   before_action :set_playlists, only: :index
 
   def index
-    @q = SongCollection.new(params: search_params).q
+    @q = Song.ransack(search_params)
     @q.sorts = "title asc" if @q.sorts.empty?
-    @songs = @q.result(distinct: true).page(params[:page])
+    @songs = @q.result.page(params[:page])
   end
 
   def show
@@ -14,37 +14,36 @@ class SongsController < ApplicationController
   end
 
   private
+    def raw_search_params
+      return {} unless params[:q]
 
-  def raw_search_params
-    return {} unless params[:q]
-
-    params.require(:q).permit(
-      :title_cont,
-      :composer_name_cont,
-      :title_or_lyrics_or_composer_name_cont,
-      :title_start,
-      :s,
-      :chords_present,
-      :themes_name_in,
-      :languages_id_in,
-      :categories_id_in
-    )
-  end
-
-  def search_params
-    if session[:restricted_categories] == true
-      raw_search_params
-    else
-      raw_search_params.merge categories_id_not_in: Category.restricted.map(&:id)
+      params.require(:q).permit(
+        :title_cont,
+        :composer_name_cont,
+        :title_or_lyrics_or_composer_name_cont,
+        :title_start,
+        :s,
+        :chords_present,
+        :themes_name_in,
+        :languages_id_in,
+        :categories_id_in
+      )
     end
-  end
 
-  def set_categories
-    @categories =
-      if session[:restricted_categories]
-        Category.all
+    def search_params
+      if session[:restricted_categories] == true
+        raw_search_params
       else
-        Category.unrestricted
+        raw_search_params.merge categories_id_not_in: Category.restricted.map(&:id)
       end
-  end
+    end
+
+    def set_categories
+      @categories =
+        if session[:restricted_categories]
+          Category.all
+        else
+          Category.unrestricted
+        end
+    end
 end
